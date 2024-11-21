d7da5f088caf70d77ce764d39af64f4337355af1
Emil Lindhult <emillindhult@gmail.com>
2024-11-21 17:05:05 +0100

    Created API, added some validation for creation of book, Added som tests, and getAll query

---
+++ Presentation/Controllers/BookController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Domain.Models;
+using Application.Books.Commands.CreateBook;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BookController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // GET: api/<BookController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<BookController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<BookController>
+        [HttpPost]
+        public async void Post([FromBody] Book bookToAdd)
+        {
+            await _mediator.Send(new CreateBookCommand(bookToAdd));
+        }
+
+        // PUT api/<BookController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BookController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,3 +1,5 @@
+using Application;
+using Infrastructure;
 
 namespace Presentation
 {
@@ -13,6 +15,8 @@ namespace Presentation
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddApplication().AddInfrastructure();
 
             var app = builder.Build();
 
