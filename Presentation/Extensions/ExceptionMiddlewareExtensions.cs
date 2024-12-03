f3b40ac4855aa865997382db85fb5ae7a3811a8e
Emil Lindhult <emillindhult@gmail.com>
2024-12-03 17:57:23 +0100

    added logger but not implement it

---
+++ Presentation/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Presentation.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app, ILoggerManager logger)
+        {
+
+        }
+    }
+}
